20428f9abcb6cd7ddbce44ec2393ec384095f39e
Potapov Evgeny <foxspb2012@gmail.com>
2020-07-18 09:40:30 +0300

    Исправления по замечаниям от наставника

--- js/card.js
+++ js/card.js
@@ -60,6 +60,39 @@
     document.removeEventListener('keydown', onPopupEscPress);
   };
 
+  var checkingData = function (data, cardElement) {
+    if (data.author.avatar) {
+      cardElement.querySelector('.popup__avatar').src = data.author.avatar;
+    } else {
+      cardElement.querySelector('.popup__avatar').classList.add('visually-hidden');
+    }
+
+    if (data.offer.rooms && data.offer.guests) {
+      cardElement.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнат' + window.extension.roomEnding(data.offer.rooms)
+        + ' для ' + data.offer.guests + ' гост' + window.extension.guestEnding(data.offer.guests);
+    } else {
+      cardElement.querySelector('.popup__text--capacity').classList.add('visually-hidden');
+    }
+
+    if (data.offer.checkin && data.offer.checkout) {
+      cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+    } else {
+      cardElement.querySelector('.popup__text--time').classList.add('visually-hidden');
+    }
+
+    if (data.offer.photos.length > 0) {
+      cardElement.querySelector('.popup__photos').innerHTML = fillPhoto(data.offer.photos);
+    } else {
+      cardElement.querySelector('.popup__photos').classList.add('visually-hidden');
+    }
+
+    if (data.offer.features.length > 0) {
+      fillFeatures(data.offer.features, cardElement);
+    } else {
+      cardElement.querySelector('.popup__features').classList.add('visually-hidden');
+    }
+  };
+
   var renderNewCard = function (data) {
     var mapFilters = document.querySelector('.map__filters-container');
     var template = document.querySelector('#card').content.querySelector('.map__card');
@@ -67,64 +100,21 @@
     var closeCardButton = cardElement.querySelector('.popup__close');
     closeCardButton.addEventListener('click', function () {
       closeCard();
-    });
-    var popupClose = cardElement.querySelector('.popup__close');
-    var popupAvatar = cardElement.querySelector('.popup__avatar');
-    var popupTitle = cardElement.querySelector('.popup__title');
-    var popupTextAddress = cardElement.querySelector('.popup__text--address');
-    var popupTextPrice = cardElement.querySelector('.popup__text--price');
-    var popupType = cardElement.querySelector('.popup__type');
-    var popupTextCapacity = cardElement.querySelector('.popup__text--capacity');
-    var popupTextTime = cardElement.querySelector('.popup__text--time');
-    var popupFeatures = cardElement.querySelector('.popup__features');
-    var popupDescription = cardElement.querySelector('.popup__description');
-    var popupPhotos = cardElement.querySelector('.popup__photos');
-
-    fillTextContent(popupTitle, data.offer.title);
-    fillTextContent(popupTextAddress, data.offer.address);
-    fillTextContent(popupTextPrice, data.offer.price + '₽/ночь');
-    fillTextContent(popupType, nameRealty[data.offer.type]);
-    fillTextContent(popupDescription, data.offer.description);
-
-    if (data.author.avatar) {
-      popupAvatar.src = data.author.avatar;
-    } else {
-      popupAvatar.classList.add('visually-hidden');
-    }
-
-    if (data.offer.rooms && data.offer.guests) {
-      popupTextCapacity.textContent = data.offer.rooms + ' комнат' + window.extension.roomEnding(data.offer.rooms) + ' для ' + data.offer.guests + ' гост' + window.extension.guestEnding(data.offer.guests);
-    } else {
-      popupTextCapacity.classList.add('visually-hidden');
-    }
-
-    if (data.offer.checkin && data.offer.checkout) {
-      popupTextTime.textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
-    } else {
-      popupTextTime.classList.add('visually-hidden');
-    }
-
-    if (data.offer.features.length > 0) {
-      fillFeatures(data.offer.features, cardElement);
-    } else {
-      popupFeatures.classList.add('visually-hidden');
-    }
-
-    if (data.offer.photos.length > 0) {
-      popupPhotos.innerHTML = fillPhoto(data.offer.photos);
-    } else {
-      popupPhotos.classList.add('visually-hidden');
-    }
-
-    popupClose.addEventListener('click', function () {
       closePopup();
     });
 
-    popupClose.addEventListener('keydown', function (evt) {
+    closeCardButton.addEventListener('keydown', function (evt) {
       if (evt.key === 'Enter') {
         closePopup();
       }
     });
+
+    fillTextContent(cardElement.querySelector('.popup__title'), data.offer.title);
+    fillTextContent(cardElement.querySelector('.popup__text--address'), data.offer.address);
+    fillTextContent(cardElement.querySelector('.popup__text--price'), data.offer.price + '₽/ночь');
+    fillTextContent(cardElement.querySelector('.popup__type'), nameRealty[data.offer.type]);
+    fillTextContent(cardElement.querySelector('.popup__description'), data.offer.description);
+    checkingData(data, cardElement);
 
     map.insertBefore(cardElement, mapFilters);
   };
