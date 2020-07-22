cd61c6ad2aeda48da1aec1d41e17ed509f8cdd66
Евгений <pea@uiex.ru>
2020-07-22 13:26:46 +0300

    Выполнил задание личный проект: надо подкачаться (часть 2)

--- js/data.js
+++ js/data.js
@@ -1,7 +1,12 @@
 'use strict';
 
 (function () {
-  var URL = 'https://javascript.pages.academy/keksobooking/data';
+  var HTTPMethods = {
+    POST: 'https://javascript.pages.academy/keksobooking',
+    GET: 'https://javascript.pages.academy/keksobooking/data',
+  };
+
+  var TIMEOUT = 10000;
   var statusCode = {
     OK: 200
   };
@@ -22,12 +27,34 @@
       onError('Запрос не успел выполниться за ' + (xhr.timeout / 1000) + ' секунд');
     });
     xhr.responseType = 'json';
-    xhr.timeout = 10000;
-    xhr.open('GET', URL);
+    xhr.timeout = TIMEOUT;
+    xhr.open('GET', HTTPMethods.GET);
     xhr.send();
+  };
+
+  var upload = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function () {
+      if (xhr.status === statusCode.OK) {
+        onLoad();
+      } else {
+        onError();
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError();
+    });
+    xhr.addEventListener('timeout', function () {
+      onError();
+    });
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT;
+    xhr.open('POST', HTTPMethods.POST);
+    xhr.send(data);
   };
 
   window.data = {
     load: load,
+    upload: upload,
   };
 })();
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var PIN_X = 570;
+  var PIN_Y = 375;
   var map = document.querySelector('.map');
   var mainPin = map.querySelector('.map__pin--main');
   var form = document.querySelector('.ad-form');
@@ -8,6 +10,29 @@
   var formButtons = form.querySelectorAll('.ad-form__element--submit button');
   var filter = document.querySelector('.map__filters');
   var filterControls = filter.querySelectorAll('[name]');
+  var formReset = form.querySelector('.ad-form__reset');
+
+  var onLoadSuccess = function (data) {
+    window.pin.renderPins(data);
+  };
+
+  var onLoadError = function (errorText) {
+    var node = window.extension.createElement('div', null, errorText);
+    node.style = 'position: absolute; top: 0; right: 0; left: 0; z-index: 100; padding: 5px; font-size: 16px; line-height: 16px; text-align: center; background-color: #ff6587; color: #ffffff';
+    document.querySelector('.map').prepend(node);
+    setTimeout(function () {
+      node.remove();
+    }, 5000);
+  };
+
+  var onSaveSuccess = function () {
+    window.message.showSuccess();
+    lockPage();
+  };
+
+  var onSaveError = function () {
+    window.message.showError();
+  };
 
   var onMainPinMousedown = function (evt) {
     if (evt.button === 0) {
@@ -22,33 +47,32 @@
   };
 
   var lockPage = function () {
-    window.form.setAddress();
-    window.form.setFormCapacity();
-    map.classList.add('map--faded');
-    var pins = map.querySelectorAll('.map__pin:not(.map__pin--main)');
-    if (pins.length) {
-      pins.forEach(function (pin) {
-        pin.remove();
-      });
-    }
+    window.card.closeCard();
+    window.pin.deletePins();
+    form.reset();
     form.classList.add('ad-form--disabled');
     formControls.forEach(function (control) {
       control.disabled = true;
     });
+    filter.reset();
     filterControls.forEach(function (control) {
       control.disabled = true;
     });
     formButtons.forEach(function (button) {
       button.disabled = true;
     });
+    mainPin.style.top = PIN_Y + 'px';
+    mainPin.style.left = PIN_X + 'px';
+    window.form.setAddress();
+    window.form.setFormCapacity();
     mainPin.addEventListener('mousedown', onMainPinMousedown);
     mainPin.addEventListener('keydown', onMainPinEnterPress);
   };
 
   var unlockPage = function () {
-    window.form.changeAddress();
     map.classList.remove('map--faded');
-    window.pin.renderPins();
+    window.data.load(onLoadSuccess, onLoadError);
+    window.form.changeAddress();
     form.classList.remove('ad-form--disabled');
     formControls.forEach(function (control) {
       control.disabled = false;
@@ -59,9 +83,20 @@
     filterControls.forEach(function (control) {
       control.disabled = false;
     });
+    window.form.changeAddress();
     mainPin.removeEventListener('mousedown', onMainPinMousedown);
     mainPin.removeEventListener('keydown', onMainPinEnterPress);
   };
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.data.upload(new FormData(form), onSaveSuccess, onSaveError);
+  });
+
+  formReset.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    lockPage();
+  });
 
   lockPage();
 })();
